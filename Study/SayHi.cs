1da8724786d7f0c2af8a7e544d39dcc711e9f8f9
Bohdan_Havryliuk <bogdangavrilyuk96@gmail.com>
2023-09-06 14:39:41 +0300

    added file SayHi

---
+++ Study/SayHi.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study
+{
+    internal class SayHi
+    {
+
+        public void Hi()
+        {
+            Console.WriteLine("I say Hi!");
+        }
+    }
+}
